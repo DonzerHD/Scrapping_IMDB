e99dcddff1f3d8a2b70fd3538dd9efd15d769138
Dylan Vanstavel <vanstavel.dylan@outlook.fr>
2023-04-18 16:44:14 +0200

    Enregistrer les modifications locales

--- scrapping_imdb/scrapping_imdb/items.py
+++ scrapping_imdb/scrapping_imdb/items.py
@@ -9,3 +9,16 @@ import scrapy
 class MovieItem(scrapy.Item):
     title = scrapy.Field()
     genre = scrapy.Field()
+    
+    
+class SerieItem(scrapy.Item):
+    title = scrapy.Field()
+    score = scrapy.Field()
+    # genre = scrapy.Field()
+    year = scrapy.Field()
+    # time = scrapy.Field()
+    description = scrapy.Field()
+    actor = scrapy.Field()
+    public = scrapy.Field()
+    # country = scrapy.Field()
+    
